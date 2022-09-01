25b93d7d04ce393fb549db384d70b1436d61106f
trentreimer <48562882+trentreimer@users.noreply.github.com>
2022-09-01 10:34:09 -0500

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -1,1 +1,29 @@
+{
+    // Check for a camera
+    (async function() {
+        let hasVideoDevice = false;
 
+        try {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            
+            for (const device of devices) {
+                if (device.kind === 'videoinput') {
+                    hasVideoDevice = true;
+                }
+            }
+        } catch (error) {
+            console.log('Unable to list media devices');
+        }
+
+        if (hasVideoDevice) {
+        } else {
+            document.querySelectorAll('#no-camera-msg').forEach(e => {
+                e.classList.remove('hidden');
+            });
+
+            document.querySelectorAll('.typewriter-link, .demo-link').forEach(e => {
+                e.remove();
+            });
+        }
+    })();
+}
